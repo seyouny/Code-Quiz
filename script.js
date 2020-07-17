cd792c6a9f6142a1d818700670900ce86a39ee08
Seyoung Yoon <stacey.exp@gmail.com>
2020-07-17 12:16:09 -0700

    fixed local storage

--- script.js
+++ script.js
@@ -10,35 +10,38 @@ var form = document.querySelector("#form");
 var submit = document.querySelector("#sub");
 var Names = document.querySelector("#Names");
 var names = document.querySelector("#name");
+var nameslist = document.querySelector("#highlist");
+
 Questions = [
     {
-    Q: "Which Minecraft pickaxe has the most uses?", 
-    O:["Wood Pickaxe", "Iron Pickaxe","Stone Pickaxe" , "Gold Pickaxe"],
-    A:"Iron Pickaxe"
+        Q: "Which Minecraft pickaxe has the most uses?",
+        O: ["Wood Pickaxe", "Iron Pickaxe", "Stone Pickaxe", "Gold Pickaxe"],
+        A: "Iron Pickaxe"
     },
 
     {
-    Q:"Who is Tom Nook?",
-    O:["An elephant known for grand larceny","A raccoon with unknown intentions", "A skinny man", "An uncle who hates animals"],
-    A: "A raccoon with unknown intentions"
+        Q: "Who is Tom Nook?",
+        O: ["An elephant known for grand larceny", "A raccoon with unknown intentions", "A skinny man", "An uncle who hates animals"],
+        A: "A raccoon with unknown intentions"
     },
-    
+
     {
-    Q:"Who said the following 'The Hobbits are going to Isengard?'",
-    O:["Wood Pickaxe", "Gollum", "Legolas","Frodo Baggins"],
-    A:"Legolas"
+        Q: "Who said the following 'The Hobbits are going to Isengard?'",
+        O: ["Wood Pickaxe", "Gollum", "Legolas", "Frodo Baggins"],
+        A: "Legolas"
     }]
 
 var nameList = [];
 var score = 0;
 seconds = 30;
 newseconds = 30;
+var storeNames = []
 
 lose = 0;
 function checkQuestion() {
 
-    if (this.value === Questions[questionCount].A){
-        if (Questions.length > questionCount){
+    if (this.value === Questions[questionCount].A) {
+        if (Questions.length > questionCount) {
             wrong.textContent = '';
             questionCount++;
             console.log(questionCount)
@@ -50,83 +53,91 @@ function checkQuestion() {
     else {
         lose = 1;
         loseTime(lose);
-        setTimeout(function(){wrong.textContent = "Incorrect!"},1000);
+        setTimeout(function () { wrong.textContent = "Incorrect!" }, 1000);
 
-        }     
+    }
 }
 function timer() {
-    var timerInterval = setInterval(function(){
-    seconds--;
-    newseconds--;
-    timeLeft.textContent = "Time Left: " + seconds;
-    if (seconds === 0) {
-        clearInterval(timerInterval);
-        alert("Time's up!");
-        highscore();
-    }
+    var timerInterval = setInterval(function () {
+        seconds--;
+        newseconds--;
+        timeLeft.textContent = "Time Left: " + seconds;
+        if (seconds <= 0) {
+            clearInterval(timerInterval);
+            alert("Time's up!");
+            highscore();
+            var k = localStorage.getItem("Namess");
+            for (i in k) {
+                Names.textContent = k;
+            }
+            timeLeft.textContent = "Time Left: 0";
+        }
 
-    },1000);
+    }, 1000);
 }
-    
-    function loseTime(lose){
-        if (lose === 1){
-            alert(lose)
-            console.log(newseconds)
-            seconds = seconds-10;
-            lose = 0;
 
-            return seconds;
-        }
-        else{
-            return seconds;
-        }
+function loseTime(lose) {
+    if (lose === 1) {
+        console.log(newseconds)
+        seconds = seconds - 10;
+        lose = 0;
+
+        return seconds;
     }
+    else {
+        return seconds;
+    }
+}
 
 
 var questionCount = 0;
-function buildQuiz(){
-    if (questionCount < Questions.length){
-    container.innerHTML = "";
-    Question.textContent = Questions[questionCount].Q;
-   // Options.textContent = Questions[questionCount].O;
-    Questions[questionCount].O.forEach(element => {
-        var button = document.createElement("button");
+function buildQuiz() {
+    if (questionCount < Questions.length) {
+        container.innerHTML = "";
+        Question.textContent = Questions[questionCount].Q;
+        // Options.textContent = Questions[questionCount].O;
+        Questions[questionCount].O.forEach(element => {
+            var button = document.createElement("button");
 
-        button.setAttribute("value",element);
-        button.innerHTML = element;
-        button.onclick = checkQuestion;
-        container.appendChild(button);
-    });
+            button.setAttribute("value", element);
+            button.innerHTML = element;
+            button.onclick = checkQuestion;
+            container.appendChild(button);
+        });
     }
-    else{
+    else {
         // window.location.href = "hs.html";
         highscore();
     }
 }
 
+function highscore() {
 
-function highscore(){
     Question.textContent = "High Scores"
     container.innerHTML = '';
-   form.style.visibility = "visible";
-    var strings = names.value + " With the highest score of: " + score;
-   submit.addEventListener("click",function(){
-    Names.textContent = strings;
-       Names.innerHTML(strings);
-    Names.append(strings);
-    newseconds=0;
-   })
-//    $("#sub").submit(function(){
-//        console.log($("#name").val());
-        // $("#Names").text($("#name").val());
-//    }
-//    <h3 style= "text-align: center;" id= "Names"></h3>
+    form.style.visibility = "visible";
 
-//    <form id= "form"> 
-//         <input type="text" name="name" id="name"> 
-//         <br><input type="submit" value="Submit" id= "sub"> 
-//         </form>
-   
+    submit.addEventListener("click", function () {
+        var strings = (names.value + " With a score of: " + score + '\n ');
+        storeNames.push(strings)
+        localStorage.setItem("Namess", storeNames)
+        Names.append(strings)
+        //    Names.append(strings);
+        newseconds = 0;
+    })
+
+
+    // //    $("#sub").submit(function(){
+    //        console.log($("#name").val());
+    // $("#Names").text($("#name").val());
+    //    }
+    //    <h3 style= "text-align: center;" id= "Names"></h3>
+
+    //    <form id= "form"> 
+    //         <input type="text" name="name" id="name"> 
+    //         <br><input type="submit" value="Submit" id= "sub"> 
+    //         </form>
+
     // container.innerHTML = '';
     // Question.textContent = "High Scores"
     // inputTag.style.visibility = "visible";
@@ -143,12 +154,12 @@ function highscore(){
 
 }
 
-startGame.addEventListener("click",function() {
+startGame.addEventListener("click", function () {
     timer();
     startGame.style.visibility = "hidden";
     clear.style.visibility = "hidden";
     buildQuiz();
-    
+
     //questionCount++;
 })
 
